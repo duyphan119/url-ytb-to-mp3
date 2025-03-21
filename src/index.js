7e1832c510f02229232f67be642afba0eba45010
duyphan119 <duy.910.2015@gmail.com>
2025-03-21 16:03:42 +0700

    log catch

--- src/index.js
+++ src/index.js
@@ -27,20 +27,21 @@ app.get("/download", async (req, res) => {
     if (!fs.existsSync(songsPath)) {
       fs.mkdirSync(songsPath, { recursive: true });
     }
-    const info = await ytdl.getInfo(
-      "https://youtu.be/JvB6N7loN80?si=3x_LlyCV_iZKzUs1"
-    );
-    console.log({ info });
     const name = new Date().getTime();
     ytdl("https://youtu.be/JvB6N7loN80?si=3x_LlyCV_iZKzUs1", {
       quality: "highestaudio",
     }).pipe(fs.createWriteStream(`public/songs/${name}.mp3`));
+    const info = await ytdl.getInfo(
+      "https://youtu.be/JvB6N7loN80?si=3x_LlyCV_iZKzUs1"
+    );
+    console.log({ info });
     res.json({
       ...info.player_response.videoDetails,
       url: `/songs/${name}.mp3`,
     });
   } catch (error) {
     console.log("download error", error);
+    res.json({ error });
   }
 });
 
