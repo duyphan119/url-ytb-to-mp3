0fb35137526508629acfed75629cb41349b10b29
duyphan119 <duy.910.2015@gmail.com>
2025-03-21 12:39:24 +0700

    first-commit

---
+++ src/index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const fs = require("fs");
+const cors = require("cors");
+const ytdl = require("@distube/ytdl-core");
+const path = require("path");
+
+require("dotenv").config();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "../public")));
+app.use(cors());
+
+const PORT = process.env.PORT || 3001;
+
+app.post("/download", async (req, res) => {
+  const info = await ytdl.getInfo(req.body.url);
+  const name = new Date().getTime();
+  ytdl(req.body.url, {
+    quality: "highestaudio",
+  }).pipe(fs.createWriteStream(`public/songs/${name}.mp3`));
+  res.json({
+    ...info.player_response.videoDetails,
+    url: `/songs/${name}.mp3`,
+  });
+});
+
+app.post("/delete", (req, res) => {
+  fs.unlinkSync(path.join(__dirname, `../public${req.body.url}`));
+  res.json({ message: "Deleted highestaudio.mp3" });
+});
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${3001}`);
+});
